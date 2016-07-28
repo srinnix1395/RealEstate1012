6b24062d072aedc7d20d667a05813a5f78bb0d2c
Srinnix <srinnix13@gmail.com>
2016-07-28 17:01:07 +0700

    setup gradle

--- app/src/main/java/com/qtd/realestate1012/MainActivity.java
+++ app/src/main/java/com/qtd/realestate1012/MainActivity.java
@@ -1,7 +1,9 @@
 package com.qtd.realestate1012;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
