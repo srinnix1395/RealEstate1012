dc584fe7ee48d3e7c08e1341e76b204f873d88ac
Srinnix <srinnix13@gmail.com>
2016-07-28 19:25:40 +0700

    login_activity_layout

---
+++ app/src/main/java/com/qtd/realestate1012/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.qtd.realestate1012.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qtd.realestate1012.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Dell on 7/28/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login_id);
+        ButterKnife.bind(this);
+    }
+}
