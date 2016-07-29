05dc47f0c64d5f008f396bcc8a251ab70dc12d5b
Srinnix <srinnix13@gmail.com>
2016-07-29 23:24:31 +0700

    login_activity_layout

--- app/src/main/java/com/qtd/realestate1012/activity/LoginActivity.java
+++ app/src/main/java/com/qtd/realestate1012/activity/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.qtd.realestate1012.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
