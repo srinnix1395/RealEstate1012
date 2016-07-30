7db8a5168355371edda83e83c52c27d9acfa7468
Srinnix <srinnix13@gmail.com>
2016-07-30 22:27:08 +0700

    main_activity

--- app/src/main/java/com/qtd/realestate1012/activity/MainActivity.java
+++ app/src/main/java/com/qtd/realestate1012/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.qtd.realestate1012.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
 
 import com.qtd.realestate1012.R;
 import com.qtd.realestate1012.constant.AppConstant;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private SearchFragment searchFragment;
     private FavoriteFragment favoriteFragment;
     private NotificationFragment notificationFragment;
+    private PopupMenu popupMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 switch (tab.getPosition()) {
                     case AppConstant.LAST_TAB: {
-
+                        showPopUpMenu();
                         break;
                     }
                     default: {
@@ -60,12 +64,43 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+                if (tab.getPosition() == AppConstant.LAST_TAB) {
+                    showPopUpMenu();
+                }
             }
         });
+
         homeFragment = new HomeFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.layoutMain, homeFragment);
         transaction.commit();
+    }
+
+    private void showPopUpMenu() {
+        if (popupMenu == null) {
+
+
+            popupMenu = new PopupMenu(this, findViewById(R.id.viewAnchor));
+            popupMenu.getMenuInflater().inflate(R.menu.menu_main, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.miLogin: {
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            break;
+                        }
+                        case R.id.miSetting: {
+
+                            break;
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+        popupMenu.show();
     }
 
     private void showFragment(TabLayout.Tab tab) {
