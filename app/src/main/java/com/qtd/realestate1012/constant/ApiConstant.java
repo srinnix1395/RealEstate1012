b6677988a8519d01160ec674966d5762b046bcd5
Srinnix <srinnix13@gmail.com>
2016-08-09 23:20:56 +0700

    login

--- app/src/main/java/com/qtd/realestate1012/constant/ApiConstant.java
+++ app/src/main/java/com/qtd/realestate1012/constant/ApiConstant.java
@@ -4,16 +4,46 @@ package com.qtd.realestate1012.constant;
  * Created by Dell on 7/29/2016.
  */
 public class ApiConstant {
-    public static final String _ID = "ID";
-    public static final String EMAIL = "Email";
-    public static final String PASSWORD = "Password";
-    public static final String NAME = "Name";
+    //    database properties
+    public static final String _ID = "_id";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String NAME = "name";
     public static final String DOB = "DOB";
-    public static final String TELEPHONE = "Telephone";
-    public static final String ADDRESS = "Address";
-    public static final String GENDER = "Gender";
-    public static final String AVATAR = "Avatar";
-    public static final String TYPE = "Type";
-    public static final String _ID_SOCIAL = "_Id_social";
-    public static final String NOTIFICATION_ID = "RegID";
+    public static final String TELEPHONE = "telephone";
+    public static final String ADDRESS = "address";
+    public static final String GENDER = "gender";
+    public static final String AVATAR = "avatar";
+    public static final String TYPE = "type";
+    public static final String _ID_SOCIAL = "_id_social";
+    public static final String NOTIFICATION_ID = "regID";
+
+
+    public static final String RESULT = "result";
+    public static final String FAILED = "failed";
+    public static final String LIST_HOUSE = "list_house";
+    public static final String SUCCESS = "success";
+    public static final String PRICE = "price";
+    public static final String IMAGE = "image";
+    public static final String IMAGE_NO1 = "no1";
+
+
+    //    api place
+    public static final String API_PLACE_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    public static final String API_KEY = "AIzaSyAXKdlSDdIttYHwoyC2Ss_BqQa1HETpbBs";
+    public static final String API_PLACE_TYPE_SCHOOL = "school";
+    public static final String API_DIRECTION_URL = "http://maps.googleapis.com/maps/api/directions/xml?";
+    public static final int DEFAULT_RADIUS = 5000;
+    public static final String API_PLACE_STATUS_SUCCESS = "OK";
+    public static final String API_PLACE_STATUS_FAILED = "REQUEST_DENIED";
+    public static final String API_PLACE_STATUS = "status";
+    public static final String API_PLACE_KEY_RESULTS = "results";
+    public static final String API_PLACE_GEOMETRY = "geometry";
+    public static final String API_PLACE_LOCATION = "location";
+    public static final String API_PLACE_LATITUDE = "lat";
+    public static final String API_PLACE_LONGITUDE = "lng";
+    public static final String API_PLACE_PLACE_ID = "place_id";
+    public static final String API_PLACE_DATA = "data";
+    public static final String API_PLACE_KEY_TYPE = "type";
+    public static final String API_PLACE_TYPE_HOSPITAL = "hospital";
 }
